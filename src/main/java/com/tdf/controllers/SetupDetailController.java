afe3405592a4b0f3e9286d3d527ed1e2927ffcfd
sifonezzz <leandroabarcac@gmail.com>
2025-08-17 19:20:18 -0400

    Small changes.

--- src/main/java/com/tdf/controllers/SetupDetailController.java
+++ src/main/java/com/tdf/controllers/SetupDetailController.java
@@ -4,13 +4,20 @@ import com.tdf.Controller;
 import com.tdf.MainApp;
 import com.tdf.data.SetupSample;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 
 public class SetupDetailController implements Controller {
 
     @FXML private Label setupNameLabel;
     @FXML private TextArea descriptionArea;
+    @FXML private Button editDescriptionButton;
+    @FXML private Button saveDescriptionButton;
+    @FXML private VBox descriptionContainer;
     
     private MainApp mainApp;
     private SetupSample currentSetup;
@@ -20,18 +27,37 @@ public class SetupDetailController implements Controller {
         this.mainApp = mainApp;
     }
     
-    // This method is called to pass the selected setup from the previous screen
     public void setSetup(SetupSample setup) {
         this.currentSetup = setup;
         setupNameLabel.setText(setup.getName());
         descriptionArea.setText(setup.getDescription());
+        setEditMode(false); // Start in view mode
+    }
+
+    @FXML
+    private void handleEditDescription() {
+        setEditMode(true);
     }
 
     @FXML
     private void handleSaveDescription() {
         currentSetup.setDescription(descriptionArea.getText());
         MainApp.getDataManager().saveSetupSamples();
-        // Optionally, show a confirmation message
+        setEditMode(false);
+    }
+
+    private void setEditMode(boolean editing) {
+        descriptionArea.setEditable(editing);
+        editDescriptionButton.setVisible(!editing);
+        saveDescriptionButton.setVisible(editing);
+        
+        if (editing) {
+            descriptionArea.getStyleClass().remove("non-editable-text");
+            descriptionArea.getStyleClass().add("editable-text");
+        } else {
+            descriptionArea.getStyleClass().remove("editable-text");
+            descriptionArea.getStyleClass().add("non-editable-text");
+        }
     }
 
     @FXML
@@ -48,5 +74,4 @@ public class SetupDetailController implements Controller {
     private void handleBack() {
         mainApp.showSetups();
     }
-
 }
